4f2dc57c81f8bbd02a5a5507a026272caeca6459
RijansPatoliya <rijans.patoliya.cg@gmail.com>
2025-05-13 09:16:27 +0530

    fix issue

--- Backend/Config/db.js
+++ Backend/Config/db.js
@@ -3,17 +3,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const connectDB = async () => {
-    try{
-        await mongoose.connect(process.env.MONGO_URI,{
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
+    try {
+        await mongoose.connect(process.env.MONGO_URI); // Removed deprecated options
         console.log("MongoDB Connected");
-    }catch(error){
+    } catch (error) {
         console.error(`Error: ${error.message}`);
         process.exit(1);
     }
-}
+};
 
 export default connectDB;
-
